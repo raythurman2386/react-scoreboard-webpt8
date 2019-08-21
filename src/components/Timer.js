567a284dc13e0ca65ac948736157c02f1c4d8afe
Raymond Thurman <raymondthurman5@gmail.com>
2019-08-21 10:57:03 -0400

    timer properly functional
    timer working properly and rendering properly, needs functionality to stop at 00:00

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,45 +1,47 @@
-import React, { useState, useEffect, useRef } from 'react';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import "./App.css";
 
 // custom useInterval hook setup
 function useInterval(callback, delay) {
-	const savedCallback = useRef();
+  const savedCallback = useRef();
 
-	// Remember the latest callback.
-	useEffect(
-		() => {
-			savedCallback.current = callback;
-		},
-		[ callback ],
-	);
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
 
-	// Set up the interval.
-	useEffect(
-		() => {
-			function tick() {
-				savedCallback.current();
-			}
-			if (delay !== null) {
-				let id = setInterval(tick, delay);
-				return () => clearInterval(id);
-			}
-		},
-		[ delay ],
-	);
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
 }
 
-const Timer = (props) => {
-	// hook for the timer
-	const [ minutes, setMinutes ] = useState(10);
-	const [ seconds, setSeconds ] = useState(0);
+const Timer = props => {
+  // hook for the timer
+  const [minutes, setMinutes] = useState(10);
+  const [seconds, setSeconds] = useState(0);
 
-	// useInterval for the timer
-	useInterval(() => {
-		seconds <= 0 ? setMinutes(minutes - 1) && setSeconds(59) : setSeconds(seconds - 1);
-	}, 1000);
+  // useInterval for the timer
+  useInterval(() => {
+    if (seconds <= 0) {
+      setMinutes(minutes - 1);
+      setSeconds(59);
+    } else {
+      setSeconds(seconds - 1);
+    }
+  }, 1000);
 
-	// Return the timer using string interpolation to have the colon
-	return <div className='timer'>{`${minutes}:${seconds}`}</div>;
+  let newMinute = minutes < 10 ? "0" + minutes : minutes;
+  let newSecond = seconds < 10 ? "0" + seconds : seconds;
+
+  // Return the timer using string interpolation to have the colon
+  return <div className="timer">{`${newMinute}:${newSecond}`}</div>;
 };
 
 export default Timer;
