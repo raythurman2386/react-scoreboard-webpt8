de42f6e144002c6278c16ea7b94590c291512886
Raymond Thurman <raymondthurman5@gmail.com>
2019-08-21 09:20:12 -0400

    useEffect in place but not working
    implemented the useEffect hook for the timer, need to sit down and look into why it's inoperable when I'm not at work

--- src/components/TopRow.js
+++ src/components/TopRow.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Timer from "./Timer";
+import "./App.css";
 
 const TopRow = props => {
   return (
-    <div className='topRow'>
-      <div className='home'>
-        <h2 className='home__name'>{props.homeName}</h2>
+    <div className="topRow">
+      <div className="home">
+        <h2 className="home__name">{props.homeName}</h2>
 
         {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-        <div className='home__score'>{props.home}</div>
+        <div className="home__score">{props.home}</div>
       </div>
-      <div className='timer'>00:03</div>
-      <div className='away'>
-        <h2 className='away__name'>{props.awayName}</h2>
-        <div className='away__score'>{props.away}</div>
+      <Timer min={props.min} sec={props.sec} />
+      <div className="away">
+        <h2 className="away__name">{props.awayName}</h2>
+        <div className="away__score">{props.away}</div>
       </div>
     </div>
   );
