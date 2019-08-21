6464963e67948766e75d4238e5411e0230614076
raythurman2386 <raymondthurman5@gmail.com>
2019-08-20 21:47:26 -0400

    home buttons broken down to their component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import './App.css';
 import TopRow from './TopRow';
 import BottomRow from './BottomRow';
+import HomeButtons from './Buttons/HomeButtons';
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
@@ -16,15 +17,16 @@ function App() {
         <BottomRow />
       </section>
       <section className='buttons'>
-        <div className='homeButtons'>
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
+        <HomeButtons touchdown={() => setHomeScore(homeScore + 7)} fieldgoal={() => setHomeScore(homeScore + 3)} />
+        {/* <div className='homeButtons'>
+          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. 
           <button className='homeButtons__touchdown' onClick={() => setHomeScore(homeScore + 7)}>
             Home Touchdown
           </button>
           <button className='homeButtons__fieldGoal' onClick={() => setHomeScore(homeScore + 3)}>
             Home Field Goal
           </button>
-        </div>
+        </div> */}
         <div className='awayButtons'>
           <button className='awayButtons__touchdown' onClick={() => setAwayScore(awayScore + 7)}>
             Away Touchdown
